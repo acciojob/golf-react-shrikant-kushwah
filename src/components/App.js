50ebdc95c267e78494199f6053fc19bde604c874
shrikant-kushwah <shrikant20052001@gmail.com>
2025-07-18 12:25:29 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,62 @@
-import React, { Component } from "react";
-import "../styles/App.css";
+import React, { Component, useState } from "react";
+import '../styles/App.css';
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       renderBall: false,
       posi: 0,
       ballPosition: { left: "0px" }
     };
-    this.renderChoice = this.renderBallOrButton.bind(this);
-    this.buttonClickHandler = this.buttonClickHandler.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-  }
+    this.renderChoice = this.renderBallOrButton.bind(this)
+    this.buttonClickHandler = this.buttonClickHandler.bind(this)
+    this.handleKeyDown = this.handleKeyDown.bind(this)
+  };
 
   buttonClickHandler() {
     this.setState({ renderBall: true }, () => {
-      // Focus the div to ensure key event works in Cypress
-      const ball = document.getElementById("ball");
-      if (ball) ball.focus();
-    });
+      const ball = document.getElementById("ball")
+      if (ball) ball.focus()
+    })
   }
-
   renderBallOrButton() {
     if (this.state.renderBall) {
-      return (
-        <div
-          className="ball"
-          id="ball"
-          tabIndex="0" // Make it focusable for Cypress
-          style={this.state.ballPosition}
-        ></div>
-      );
+      return <div className="ball" id="ball" tabIndex="0" style={this.state.ballPosition}></div>
     } else {
-      return (
-        <button
-          className="start"
-          id="start"
-          onClick={this.buttonClickHandler}
-        >
-          Start
-        </button>
-      );
+      return <button className="start" id="start" onClick={this.buttonClickHandler}>Start</button>
     }
   }
 
+  //handle arrow right key
   handleKeyDown(event) {
     if (!this.state.renderBall) return;
-    if (event.key === "ArrowRight") {
-      const newPosition = this.state.posi + 5;
-      this.setState({
-        posi: newPosition,
-        ballPosition: { left: `${newPosition}px` }
-      });
+    if (event.key === "ArrowRight" || event.keyCode === 39) {
+      this.setState(((preState) => {
+        const newPosition = preState.posi + 5;
+        return {
+          posi: newPosition,
+          ballPosition: { left: `${newPosition}px` }
+        }
+      }))
     }
   }
 
+  // bind ArrowRight keydown event
   componentDidMount() {
-    window.addEventListener("keydown", this.handleKeyDown);
+    window.addEventListener("keydown", this.handleKeyDown)
   }
 
   componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleKeyDown);
+    window.removeEventListener("keydown", this.handleKeyDown)
   }
 
   render() {
-    return <div className="playground">{this.renderBallOrButton()}</div>;
+    return (
+      <div className="playground">
+        {this.renderBallOrButton()}
+      </div>
+    )
   }
 }
 
